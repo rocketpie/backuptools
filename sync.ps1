4b5ccb4657f3a1854a803df47820a3897b604646
Lars Conrad <mail.l.conrad@gmail.com>
2017-07-10 23:17:02 +0200

    current dev status

--- sync.ps1
+++ sync.ps1
@@ -13,52 +13,129 @@
 #>
 [CmdletBinding()]
 Param(
-    [Parameter]
+    #[Parameter(Mandatory=$true)]
+    [Parameter()]
     [string]
-    $Source,
+    $SourcePath = 'C:\D\tmp\test\src',
 
-    [Parameter]
+    #[Parameter(Mandatory=$true)]
+    [Parameter()]
     [string]
-    $Target
+    $TargetPath = 'C:\D\tmp\test\target'
 )
 
-$Source = Resolve-Path $Source
-$Target = Resolve-Path $Target
+$DebugIndent = 15
+$Debug = $false
+if($PSBoundParameters['Debug']){
+	$DebugPreference = 'Continue'
+    $Debug = $true
+}
 
-function New-HashedFile {
-    Param( [Parameter(ValueFromPipeline=$true)]$File, $Source, $ReadHashFromFile=$false)
-    $result = New-Object System.Management.Automation.PSCustomObject
-    $result | Add-Member -TypeName NoteProperty -Name 'File' -Value $File    
+$Source = Resolve-Path $SourcePath
+$Target = Resolve-Path $TargetPath
 
-    $pathFromSource = $File.Directory.FullName.Remove(0, $Source.length)
-    if($pathFromSource.length -eq 0){ $pathFromSource = '.' }
-    $result | Add-Member -TypeName NoteProperty -Name 'RelativePath' -Value $pathFromSource
+function DebugString { Param($Object)
+    if($Object -eq $null) { ""; return }
 
-    if($ReadHashFromFile) {
-        $result | Add-Member -TypeName NoteProperty -Name 'Hash' -Value (Get-FileHash $File).Hash
+    switch($Object.GetType().Name) {
+        "DictionaryEntry" {
+            $name = "$($Object.Name)"
+            $value = "$($Object.Value)"
+            if($name.Length -gt 30) { $name = $name.SubString(0,30) + '...' }
+            if($value.Length -gt 30) { $value = $value.SubString(0,30) + '...' }
+            "$($name): $value"            
+        }
+        "PathInfo" {
+            "$($Object.Path)"
+        }
+
+        default {
+            "DebugString: unknown Type $($Object.GetType().FullName)"            
+        }
     }
 }
 
-$sourceFiles = @{}
-ls -r $Source | New-HashedFile $Source | %{ if(-not $sourceFiles.ContainsKey($_.Hash)) { $sourceFiles.Add($_.Hash, $_) } }
+function DebugVar { 
+    Param($Name, $Value)
+    if(-not $Debug) { return }
 
-$latestFolder = Join-Path $Target "_latest"
-$backupFolder = Join-Path $Target "$(Get-Date -Format 'yyyyMMdd')"
-while (Test-Path $backupRootFolder) {
-    if($backupRootFolder -match '\.(\d+)$'){
-        $nextRevision = $Matches[1] + 1;
-        $backupRootFolder -replace '\.\d+$', ".$nextRevision"
+    if($Value -eq $null) { $Value = (ls variable:$Name).Value }
+    if(($Value.GetType().FullName -ne 'System.String') -and ($Value.GetEnumerator -is [System.Management.Automation.PSMethodInfo])) {
+        $enumerator = $Value.GetEnumerator()
+        $enumerator.MoveNext()
+        Write-Debug "$(("$" + $Name).PadRight($DebugIndent)): $(DebugString $enumerator.Current)"	
+        while($enumerator.MoveNext()) {
+            Write-Debug "$(''.PadRight($DebugIndent)): $(DebugString $enumerator.Current)"	
+        }
     }
     else {
-        $backupRootFolder = "$backupRootFolder.1"
+        Write-Debug "$($Name.PadRight($DebugIndent)): $(DebugString $Value)"	
+    }
+}
+
+function New-HashedFile {
+    Param( 
+        [Parameter(ValueFromPipeline=$true)]
+        [System.IO.FileInfo]
+        $File, 
+        
+        [Parameter(Mandatory=$true, Position=1)]
+        [System.Management.Automation.PathInfo]
+        $Source, 
+
+        [Parameter()]
+        [switch]
+        $ReadHashFromFile=$false
+    )
+    Begin { }
+    Process {
+        #DebugVar File $File.FullName
+        #DebugVar Source
+        #DebugVar ReadHashFromFile
+
+        $result = New-Object PSObject
+        $result | Add-Member -MemberType NoteProperty -Name 'File' -Value $File    
+
+        $pathFromSource = $File.Directory.FullName.Remove(0, $Source.Path.Length)
+        if($pathFromSource.Length -eq 0) { $pathFromSource = '.' }
+        #DebugVar pathFromSource
+        $result | Add-Member -MemberType NoteProperty -Name 'RelativePath' -Value $pathFromSource        
+
+        if($ReadHashFromFile) {
+            $result | Add-Member -MemberType NoteProperty -Name 'Hash' -Value (gc $File.FullName)[0]
+        }
+        else { 
+            $result | Add-Member -MemberType NoteProperty -Name 'Hash' -Value (Get-FileHash $File.FullName).Hash
+        }
+
+        $result
+    }
+    End { }
+}
+
+DebugVar Source
+DebugVar Target
+
+$sourceFiles = @{}
+ls -Recurse -File $Source | New-HashedFile $Source | %{ if(-not $sourceFiles.ContainsKey($_.Hash)) { $sourceFiles.Add($_.Hash, $_) } }
+
+DebugVar sourceFiles
+
+$latestFolder = Join-Path $Target '_latest'
+$backupFolder = Join-Path $Target "$(Get-Date -Format 'yyyyMMdd')"
+while (Test-Path $backupFolder) {
+    if($backupFolder -match '\.(\d+)$'){
+        $nextRevision = $Matches[1] + 1;
+        $backupFolder -replace '\.\d+$', ".$nextRevision"
+    }
+    else {
+        $backupFolder += '.1'
     }
 }
 
 $targetFiles = @{}
-ls -r $latestFolder | New-HashedFile $latestFolder -ReadHashFromFile | %{ $targetFiles.Add($_.RelativePath, $_) }
+if(-not (Test-Path $latestFolder)) { New-Object }
+ls -Recurse -File $latestFolder | New-HashedFile $latestFolder -ReadHashFromFile | %{ $targetFiles.Add($_.RelativePath, $_) }
 
 
-
-
-
-
+DebugVar targetFiles 
