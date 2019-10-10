a418f554971b4db25adf2d2407326869d7059f0d
Lars Conrad <mail.l.conrad@gmail.com>
2019-10-11 00:01:03 +0200

    refactor Compare-Directories.ps1

--- Test/Compare-Directories.ps1
+++ Test/Compare-Directories.ps1
@@ -10,6 +10,23 @@
         Option B: git style ignore directories, compare files only
         Option C: ignore, run into derived problems later.
 
+        Option A sounds the smartest here, but poses a few problems:
+        first, the copy procedure needs to be way more elaborate, because cp src/dir bck/dir also copies all the files inside.
+        So cp src/newdir might be easy, simply skipping everything under newdir, updates and rm are harder.
+        rm might ignore all errors on rm newdir/file after newdir has been deleted
+        update will need to ignore newdir.
+
+        that means cp $item needs to behave differently for files and dirs requiring lots of if's.
+        Option B shines here, ignoring dirs eliminates if(isDir) {} altogether.
+        
+        Enter Option B2: ignore directories + treat empty directories as files
+        an empty directory does not pose any of the mentioned challenges.
+        It needs never be included in the Update selection and can be cp'ed and rm'd like a file.
+
+        right?
+        What if the directory is only empty on one side?
+
+
     .EXAMPLE
         Compare-Directories \sourcedir \targetdir
 #>
@@ -24,6 +41,70 @@ Param(
     $TargetPath
 )
 
+class DiffData {
+    DiffData() {
+        $this.Enter = [System.Collections.Generic.List[string]]::new()
+        $this.Update = [System.Collections.Generic.List[string]]::new()
+        $this.Exit = [System.Collections.Generic.List[string]]::new()
+    }
+
+    [int]$Total
+    [System.Collections.Generic.List[string]]$Enter
+    [System.Collections.Generic.List[string]]$Update
+    [System.Collections.Generic.List[string]]$Exit
+}
+
+# $compareItems: function($item) { [bool] } predicate: return if items present in source and target differ
+function DiffList([string[]]$sourceList, [string[]]$targetList, $ItemsDiffer) {
+    $result = New-Object DiffData
+    
+    # step through both sorted lists in sync, sorting mismatches on the go
+    $sIdx = 0;
+    $tIdx = 0;
+    $hasNext = ($sourceList.Length -gt 0) -and ($targetList.Length -gt 0)   
+    while ($hasNext) {
+        $sourceItem = $sourceList[$sIdx]
+        $targetItem = $targetList[$tIdx]
+        $compare = $sourceItem.CompareTo($targetItem)
+
+        Write-Debug "'$sourceItem'.CompareTo('$targetItem'): $compare"
+        if ($compare -eq 0) {
+            $sIdx++;
+            $tIdx++;
+            if([bool](&$ItemsDiffer $sourceItem)) {
+                $result.Update.Add($sourceItem)
+            }
+        }
+        elseif ($compare -gt 0) {
+            # 1 == 'b'.CompareTo('a') => source is ahead of target, indicating a file in target that's missing in source
+            $result.Exit.Add($targetItem)
+            $tIdx++;
+        }
+        else <# ($compare -lt 0) #> {
+            # -1 == 'a'.CompareTo('b') => source is behind on target, indicating an extra file in source
+            $result.Enter.Add($sourceItem)
+            $sIdx++;
+        }
+            
+        $hasNext = ($sourceList.Length -gt $sIdx) -and ($targetList.Length -gt $tIdx)
+    }
+
+    # flush remainder
+    $remainingSourceFiles = $sourceFiles[$sIdx..$sourceFiles.Length]
+    if ($remainingSourceFiles) {
+        $result.Enter.AddRange(@($remainingSourceFiles))
+    }
+        
+    $remainingTargetFiles = $targetFiles[$tIdx..$targetFiles.Length]
+    if ($remainingTargetFiles) {
+        $result.Exit.AddRange(@($remainingTargetFiles))
+    }
+
+    $result.Total = $result.Enter.Count + $result.Update.Count + $result.Exit.Count
+    $result
+}
+
+
 $ecnt = $Error.Count
 
 $SourceDir = get-item $SourcePath
@@ -31,55 +112,36 @@ $targetDir = get-item $targetPath
 
 if ($Error.Count -ne $ecnt) { exit } # can't find directories or somethin
 
-$result = New-Object psobject -Property @{
-    'Total' = 0; # total number of differences
-    'Enter' = @(); # files added in $source, not present in $target
-    'Exit'  = @() # files not in #source, but present in $target
-};
-
 # get sorted lists of both directories
 # remove common root path (including '\' that was not part of the dir.FullName) to get comparable relative names. 
-# They come sorted from ls, but for good measure, ensure alphabetic sorting.
-$sourceFiles = @(ls -Recurse $SourceDir | %{ $_.FullName.Substring($SourceDir.FullName.Length + 1) } | sort)
-$targetFiles = @(ls -Recurse $targetDir | %{ $_.FullName.Substring($targetDir.FullName.Length + 1) } | sort)
-    
-# step through both sorted lists in sync, sorting mismatches on the go
-$sIdx = 0;
-$tIdx = 0;
-$hasNext = ($sourceFiles.Length -gt 0) -and ($targetFiles.Length -gt 0)   
-while ($hasNext) {
-    $compare = $sourceFiles[$sIdx].CompareTo($targetFiles[$tIdx])
+#$sourceDirs = @(ls -Recurse -Directory $SourceDir | %{ $_.FullName.Substring($SourceDir.FullName.Length + 1) })
+#$targetDirs = @(ls -Recurse -Directory $SourceDir | %{ $_.FullName.Substring($SourceDir.FullName.Length + 1) })
 
-    Write-Debug "'$($sourceFiles[$sIdx])'.CompareTo('$($targetFiles[$tIdx])'): $compare"
-    if ($compare -eq 0) {
-        $sIdx++;
-        $tIdx++;
+#$dirDiff = DiffList $sourceDirs $targetDirs { $false } # directories don't change once they exist
+
+$sourceFiles = @(ls -Recurse -File $SourceDir | %{ $_.FullName.Substring($SourceDir.FullName.Length + 1) })
+$targetFiles = @(ls -Recurse -File $targetDir | %{ $_.FullName.Substring($targetDir.FullName.Length + 1) })
+
+$fileDiff = DiffList $sourceFiles $targetFiles { 
+    Param($filename)
+    $sourceLength = Get-ItemPropertyValue (Join-Path $SourcePath $filename) -Name Length
+    $targetLength = Get-ItemPropertyValue (Join-Path $TargetPath $filename) -Name Length
+
+    if($sourceLength -ne $targetLength) {
+        Write-Debug "different Length"
+        return $true;
     }
-    elseif ($compare -gt 0) {
-        # 1 == 'b'.CompareTo('a') => source is ahead of target, indicating a file in target that's missing in source
-        $result.Exit += @($targetFiles[$tIdx])
-        $tIdx++;
+
+    $sourceDate = Get-ItemPropertyValue (Join-Path $SourcePath $filename) -Name LastWriteTime
+    $targetDate = Get-ItemPropertyValue (Join-Path $TargetPath $filename) -Name LastWriteTime
+
+    if($sourceDate -ne $targetDate) {
+        Write-Debug "different LastWriteTime"
+        return $true;
     }
-    else <# ($compare -lt 0) #> {
-        # -1 == 'a'.CompareTo('b') => source is behind on target, indicating an extra file in source
-        $result.Enter += @($sourceFiles[$sIdx])
-        $sIdx++;
-    }
-        
-    $hasNext = ($sourceFiles.Length -gt $sIdx) -and ($targetFiles.Length -gt $tIdx)
+
+    Write-Debug "not modified: $filename"
+    return $false;
 }
 
-# flush remainder
-$remainingSourceFiles = $sourceFiles[$sIdx..$sourceFiles.Length]
-if ($remainingSourceFiles) {
-    $result.Enter += @($remainingSourceFiles)
-}
-    
-$remainingTargetFiles = $targetFiles[$tIdx..$targetFiles.Length]
-if ($remainingTargetFiles) {
-    $result.Exit += @($remainingTargetFiles)
-}
-
-$result.Total = $result.Enter.Length + $result.Exit.Length
-
-$result
+$fileDiff
