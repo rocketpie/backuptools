f824c519e1ce6fd1939abb0f4dbb3690ab3a500e
rocketpie <mail.l.conrad@gmail.com>
2021-12-18 19:37:57 +0100

    fix 2 pages
    script was not working with only two pages
    because pages wasn't an array in that case...

--- Scan-Backside.ps1
+++ Scan-Backside.ps1
@@ -13,19 +13,24 @@
 Param(	
 )
 
+if($PSBoundParameters["Debug"]){
+	$DebugPreference = "Continue"
+}
+
 $pages = ls 'Bild*.jpg' | %{ 
 	if($_.Name -match 'Bild \((\d+?)\)') { 
 		New-Object psobject -Property @{ 'page'=$_; 'num'=[int]$Matches[1] } 
 	} 
 }
 
-$pages = $pages | sort -Property 'num' -Descending
+$pages = @($pages | sort -Property 'num' -Descending)
 $maxpage  = $pages[0].num
+Write-Debug "$maxpage ($($pages.count)) pages"
 
 for($i = 0; $i -lt $pages.Length; $i++) { 
 	$newPageIdx = ((($maxpage -$i) * 2) -1) # maxpage=4; i=0 => 7; i=1 => 5 i=2 => 3
 	$newName = $pages[$i].page.FullName -replace "\($($pages[$i].num)\)\.(\w{3})$", "($newPageIdx).`$1"
 
+	Write-Debug "mv $($pages[$i].page.FullName) $newName"
 	mv $pages[$i].page.FullName $newName
-	#"mv $($pages[$i].page.FullName) $newName"
 }
